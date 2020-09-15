eeba03d73eb17761e9f74571bbb5eb6c3d5dcc13
Ronco-note <david1997@live.com.ar>
2020-09-15 15:13:29 -0300

    first deploy

---
+++ docs/precache-manifest.5eb1e1d76835c772e1bf8bea5b024608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5d97d885026883f1489d57c8ac961a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dca98ac1442dbdc18409",
+    "url": "/static/css/main.611437bc.chunk.css"
+  },
+  {
+    "revision": "ba4014856bdcc42c2494",
+    "url": "/static/js/2.81276831.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.81276831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca98ac1442dbdc18409",
+    "url": "/static/js/main.159fb6a2.chunk.js"
+  },
+  {
+    "revision": "b63a4b8ca5166987a4a7",
+    "url": "/static/js/runtime-main.2ca0876f.js"
+  }
+]);
