8209fa3533d697167dc48740ba183ee04f9d7b26
Ronco <david1997@live.com.ar>
2021-04-11 20:25:58 -0300

    contact form added

--- src/components/content/components/contact/ContactData.js
+++ src/components/content/components/contact/ContactData.js
@@ -1,29 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
+import validator from 'validator';
+
+import useForm from '../../../../hooks/useForm';
+import { sendWithToken } from '../../../../utils/sendWithToken';
 
 
 const ContactData = ({ data }) => {
-	let email,
-		phone,
-		mailto = '';
 
-	if (data) {
-		email = data[0].email;
-		phone = data[0].cel;
-		mailto = `mailto:${email}`;
+	const [error, setError] = useState();
+
+	const [ formValues, handleInputChange, resetForm ] = useForm({
+		remitentName:"",
+		body:"",
+		remitentEmail: ""
+	});
+
+	const { remitentName, body, remitentEmail } = formValues;
+
+
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		
+		if ( validate() ) {
+			//send to backend
+			const data = {
+				name:remitentName,
+				email: remitentEmail,
+				body
+			}
+
+			sendWithToken(data,'sendEmail');
+			resetForm();
+			setError(undefined);
+		}
+
 	}
+
+	const validate = () => {
+		console.log('validate')
+		
+		if ( !validator.isEmail(remitentEmail) ) {
+			setError('No ingreso un mail valido')
+			return false
+		}
+
+		if ( remitentName === "" ) {
+			setError('Debe ingresar su nombre')
+			return false
+		}
+		
+
+		if ( remitentEmail === ""){
+			setError('Debe ingresar un email');
+			return false
+		}
+
+		if ( body === "" ) {
+			setError('Debe ingresar un mensaje')
+			return false
+		}
+
+		return true;
+	}
+
 	return (
 		<div className="contact__data">
-			<div>
-				<h3>Email:</h3>
-				<p>
-					<a href={mailto}>{email}</a>
-				</p>
-			</div>
-			<hr />
-			<div>
-				<h3>Telefono:</h3>
-				<p>{phone}</p>
-			</div>
+			<form 
+				className="contact__form"
+				onSubmit={ handleSubmit }
+			>
+				{(error)&&
+				<div className="contact__text-input error">
+					<p>{error}</p>
+				</div>
+				}
+
+				<div className="contact__text-input">
+					<label htmlFor="remitentName">Nombre</label>
+					<input
+						type="text"
+						autoComplete="off"
+						name="remitentName"
+						id="remitentName"
+						onChange={ handleInputChange }
+						value={ remitentName }
+					/>
+					<span className="contact__separator"> </span>
+				</div>
+
+				<div className="contact__text-input">
+					<label htmlFor="remitentEmail">Email</label>
+					<input
+						type="text"
+						autoComplete="off"
+						name="remitentEmail"
+						id="remitentEmail"
+						onChange={ handleInputChange }
+						value={ remitentEmail }
+					/>
+					<span className="contact__separator"> </span>
+				</div>
+
+				<div className="contact__text-input">
+					<label htmlFor="body">Mensaje</label>
+					<textarea
+						type="body"
+						autoComplete="off"
+						name="body"
+						id="body"
+						onChange={ handleInputChange }
+						value={ body }
+					/>
+					<span className="contact__separator"> </span>
+				</div>
+
+				<button
+					className="contact__button"
+				>
+					Enviar!
+				</button>
+			</form>
 		</div>
 	);
 };
---
+++ src/utils/sendWithToken.js
@@ -0,0 +1,44 @@
+import Swal from "sweetalert2";
+
+export const sendWithToken = ( data, endpoint ) => {
+    
+    Swal.fire({
+        title: 'Enviando',
+        text: 'Por favor espere...',
+        allowOutsideClick: false,
+        didOpen: () => {
+            Swal.showLoading();
+        }
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-type':'application/json',
+            'authorization': process.env.REACT_APP_API_KEY
+        },
+        body: JSON.stringify(
+            data
+        )
+    };
+
+    const url = `${process.env.REACT_APP_BACK}/${endpoint}`;
+
+    fetch(url, requestOptions)
+        .then( response => 
+            Swal.fire(
+                'Enviado!',
+                '',
+                'success'
+            )
+        )
+        .catch( error =>{ 
+            console.log(error);
+            Swal.fire(
+                'Ocurrio un problema',
+                'Por favor intente mas tarde',
+                'error'
+            )
+        });
+}
+
